7d2e56541530f014bb55000d2cb0b2822d49cc60
Stas <zimirovo13052001@yandex.ru>
2023-12-15 21:58:01 +0100

    4

--- JS/script.js
+++
@@ -1,9 +0,0 @@
-function toggleDropdown() {
-    var dropdown = document.querySelector('.dropdown');
-    var arrow = document.querySelector('.arrow');
-
-    dropdown.classList.toggle('active');
-    arrow.classList.toggle('up', dropdown.classList.contains('active'));
-}
-
-
